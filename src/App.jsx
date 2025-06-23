35022a6eccec0a98f031bb7d66a1be6bc6f1a256
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-06-23 07:42:49 +0300

    Complete UI layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import Header from "./components/Header"
 
 const App = () => {
     return (
-        <div>
-            <Header />
+        <div className="min-h-screen bg-neutral-900 text-white">
+            <div className="w-[80%] mx-auto pt-10">
+                <Header />
+
+                <div className="flex justify-between h-[85vh] gap-3">
+                    <div className="w-1/2 border h-[90%] ps-2">
+                        <h1 className="text-lg font-bold">Editor</h1>
+                    </div>
+                    <div className="w-1/2 border h-[90%] ps-2">
+                        <h1 className="text-lg font-bold">Previewer</h1>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
